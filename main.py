b293a706ef3ed5acd68379cb2b604ac450f0c9df
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-08-22 14:13:37 +0000

    Try using slack_bolt instead

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
-import logging
 import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-from flask import Flask
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-from slack_sdk import WebClient, rtm
-from slack_sdk.errors import SlackApiError
-
-from slackeventsapi import SlackEventAdapter
-
-
-logging.basicConfig(level=logging.DEBUG)
-
-app = Flask(__name__)
-
-slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
-slack_token = os.environ["SLACK_BOT_TOKEN"]
-client = WebClient(token=slack_token)
-
-slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events", app)
-
-
-# An example of one of your Flask app's routes
-@app.route("/")
-def hello():
-  return "Woah, hi there!"
-
-
-@slack_events_adapter.on("message.channels")
-def on_message(event_data):
-    print("WOAH THERE!")
-
-"""
-try:
-    response = client.chat_postMessage(
-        channel="C0P5NE354",
-        text="Hello from your app! :tada:"
-    )
-except SlackApiError as e:
-    # You will get a SlackApiError if "ok" is False
-    assert e.response["error"]    # str like 'invalid_auth', 'channel_not_found'
-"""
+@app.message("testing123")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=3000)
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
