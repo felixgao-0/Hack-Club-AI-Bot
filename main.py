e87a530ee884cf744276a66eb35a86c0697a6bee
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-08-23 17:48:09 +0000

    Fix json missing comma, start bot

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
+import json
 import os
+
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+# Initialize your app with your bot token and signing secret
+app = App(
+    token=os.environ["SLACK_BOT_TOKEN"],
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"]
+)
 
-@app.message("testing123")
+"""
+def ask_ai(question: str) -> dict:
+    url = "https://jamsapi.hackclub.dev/openai/chat/completions"
+    headers = {'Authorization': f'Bearer {os.environ["OPEN_AI_ARCADE"]}'}
+    with open('messages.json') as f:
+        prompt = json.load(f)
+    prompt.append({"role": "user", "content": question})
+    req_data = {
+        'model': 'gpt-3.5-turbo',
+        'messages': prompt,
+    }
+    r = requests.post(url, json=req_data, headers=headers)
+    return r.json()
+"""
+@app.message("hello")
 def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
+"""
+@app.message("hi")
+def question_handler(message, say):
+    print("hi, we got a message in the channel!")
+    thread_ts = message["ts"]
+    say(text=ask_ai(message["text"]), thread_ts=thread_ts) 
+"""
 
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    app.start(port=int(os.environ.get("0.0.0.0", 3000)))
