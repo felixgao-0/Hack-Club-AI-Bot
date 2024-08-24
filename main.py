fedcad7853362d5bb26ab351c164ae52b6f0b64e
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-08-24 21:32:13 +0000

    Move json to files, add middleware, wip remove btn after pressed

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import logging
 import os
 
-from slack_bolt import App
+from slack_bolt import App, BoltResponse
 
-from utils import ask_ai, is_question, get_username
+from utils import ask_ai, get_json, get_username, is_question
+
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(levelname)s - %(asctime)s: %(message)s',
+    datefmt='%H:%M:%S'  # Date format
+)
 
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
@@ -13,74 +20,26 @@ authorized = ["U07BU2HS17Z", "U07BLJ1MBEE"]
 
 @app.event("message")
 def handle_message_events(event, say, client):
-    try:
-        text = event["text"]
-    except KeyError as e:
-        print(f"Well we got an error D:\n{e}\n{event}")
-        return
-    if event["channel"] != "C07JA93AMDZ": # Test channel
-        print("Ignoring: Incorrect Channel")
-        return
-
-    if not is_question(text):
-        print("Ignoring: Not likely a question")
-        return
-
     if event.get("parent_user_id", event["user"]) != event["user"]:
         print("Ignoring: Not the thread author")
         return
 
     if event.get("subtype"):
-        print("Ignoring: Bots don't respond to bots")
+        print("Ignoring: Bot message or of subtype we don't care abt")
 
-    if event["user"] not in authorized:
-        print("Ignoring, not authorized to use bot")
-        print(event)
+    if not is_question(event["text"]):
+        print("Ignoring: Not likely a question")
         return
+
+    text = event["text"] 
 
     text_lower = text.lower()
     print(text_lower)
     if not text_lower.startswith("ai"):
         print("User did not opt for AI to respond D:")
-        block = [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Hello :wave:! I'm an AI help bot developed by Felíx. Press the button below to have me answer your question!"
-                }
-            },
-            {
-                "type": "divider"
-            },
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "plain_text",
-                        "text": "Pro Tip: Start your message with 'AI' to get a response instantly!",
-                        "emoji": True
-                    }
-                ]
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": ":blob_help: Answer My Question!",
-                            "emoji": True
-                        },
-                        "value": "yes",
-                        "action_id": "answer_question"
-                    }
-                ]
-            }
-        ]
+        block = get_json("json_data/consent_prompt.json")
         say(
-            text="Press the button below to have an AI bot answer your question!",
+            text="Press the button below to have AI respond if you need help!",
             blocks=block, 
             thread_ts=event["ts"]
         )
@@ -90,7 +49,7 @@ def handle_message_events(event, say, client):
     #response = app.client.conversations_replies(channel=event["channel"], ts=event["event_ts"])
     #print(response['messages'][0])
     
-    client.reactions_add(
+    client.reactions_add( # Loading emoji so user knows whats happening
         channel=event["channel"],
         name="spin-loading",
         timestamp=event["event_ts"]
@@ -102,27 +61,8 @@ def handle_message_events(event, say, client):
     )
 
     response_text = ask_ai(text)
-    block = [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": response_text["choices"][0]["message"]["content"]
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "context",
-            "elements": [
-                {
-                    "type": "mrkdwn",
-                    "text": "*This bot uses AI, take with a few grains of salt. Refer to the constitution as always.* Bot by Felíx. :D"
-                }
-            ]
-        }
-    ]
+    block = get_json("json_data/response_prompt.json")
+    block[0]['text']['text'] = response_text["choices"][0]["message"]["content"]
     say(
         text=response_text["choices"][0]["message"]["content"],
         blocks=block, 
@@ -136,20 +76,25 @@ def handle_message_events(event, say, client):
 
 
 @app.action("answer_question")
-def answer_question_events(ack, client, body, say):
+def answer_question_events(ack, client, body, say, logger):
     ack()
     if body['user']['id'] != body['message']['parent_user_id']:
-        print("Ignoring btn press, doesn't own action :/")
+        logger.warn("Ignoring btn press, isn't author of the thread")
         return
-
-    if body['user']['id'] not in authorized:
-        print("Ignoring btn press, not authorized")
-        return
-
-    context_data: str = ""
 
     thread_ts = body["message"]['thread_ts']
     thread_channel = body['channel']['id']
+
+    edit_block = get_json("json_data/consent_prompt.json")
+    edit_block[:-1]['elements'][0]['disabled'] = True
+    response = client.chat_update(
+        channel=body['channel']['id'],
+        ts=body['message']['ts'],
+        blocks=edit_block
+    )
+
+    context_data: str = ""
+
     response = client.conversations_replies(channel=thread_channel, ts=thread_ts)
 
     for index, message in enumerate(response["messages"]):
@@ -157,34 +102,17 @@ def answer_question_events(ack, client, body, say):
             continue
         context_data += f"{get_username(app, message['user'])} SAID: {message['text']} "
 
-    client.reactions_add(
+    client.reactions_add( # Loading emoji so user knows whats happening
         channel=thread_channel,
         name="spin-loading",
         timestamp=body['message']['ts']
     )
 
     response_text = ask_ai(response["messages"][0]['text'], context=context_data)
-    block = [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": response_text["choices"][0]["message"]["content"]
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "context",
-            "elements": [
-                {
-                    "type": "mrkdwn",
-                    "text": "*This bot uses AI, take with a few grains of salt. Refer to the constitution as always.* Bot by Felíx. :D"
-                }
-            ]
-        }
-    ]
+    
+    block = get_json("json_data/response_prompt.json")
+    block[0]['text']['text'] = response_text["choices"][0]["message"]["content"]
+
     say(
         text=response_text["choices"][0]["message"]["content"],
         blocks=block, 
@@ -197,7 +125,33 @@ def answer_question_events(ack, client, body, say):
     )
 
 
+@app.middleware
+def middleware_checks(context, next, logger):
+    logger.info("Running checks")
+
+    if context["channel_id"] != "C07JA93AMDZ": # Test channel
+        logger.warning("Ignoring, Incorrect Channel")
+        return BoltResponse(status=401, body="Incorrect channel")
+
+    #if event.get("parent_user_id", event["user"]) != event["user"]:
+    #    logger.warning("Ignoring: Not the thread author")
+    #    return
+
+    event = context.get('body', {}).get('event', {})
+    if event.get('type') == 'message':
+        subtype = event.get('subtype')
+        if subtype:
+            logger.warning("Ignoring, likely bot")
+            print(subtype)
+            return BoltResponse(status=401, body="Bot response, ignoring")
+
+    if context["user_id"] not in authorized:
+        logger.warning("Ignoring, not authorized to use bot")
+        return BoltResponse(status=401, body="Unauthorized user")
+
+    #print(context)
+
+    return next()
 
 if __name__ == "__main__":
-    print("Woah its working!")
     app.start(port=3000)
